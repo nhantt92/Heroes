823b277d92c1d42a2eb3f7a8176a89ecb48174f9
Nhan Truong <nhantt@icviet.com>
2017-11-21 17:05:18 +0700

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
